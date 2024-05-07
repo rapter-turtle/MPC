c9ea2a5d1cd35ee4c6db2af7c67ca663c3c833b6
rapter-turtle <58213518+rapter-turtle@users.noreply.github.com>
2024-05-07 16:35:11 +0900

    Add files via upload

---
+++ Vanila Ship NMPC2/Environment.py
@@ -0,0 +1,103 @@
+import numpy as np
+import math 
+from USV import *
+import time, os
+from simple_acados_settings_dev import *
+from simple_plotFcn import *
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+Tf = 100.0  # prediction horizon
+N = 100  # number of discretization steps
+T = 200.00  # maximum simulation time[s]
+dt = 1
+control_time = 1
+
+# load model
+constraint, model, acados_solver = acados_settings(Tf, N)
+
+# dimensions
+nx = model.x.size()[0]
+nu = model.u.size()[0]
+ny = nx + nu
+Nsim = int(T * N / Tf)
+
+# initialize data structs
+simX = np.ndarray((int(T/dt), nx))
+simU = np.ndarray((int(T/dt), nu))
+
+x_init = model.x0
+x0 = x_init
+tcomp_sum = 0
+tcomp_max = 0
+u0 = np.array([0, 0])
+
+
+# simulate
+for i in range(int(T/dt)):
+    if i%int(control_time/dt) == 0:
+        # print(i)
+        ################################ update reference ################################
+        for j in range(N):
+            yref = np.array([1, 0, 0, 0, 120, 0, 0, 0, 0, 0])
+            acados_solver.set(j, "yref", yref)
+        yref_N = np.array([1, 0, 0, 0, 120, 0, 0, 0])
+        acados_solver.set(N, "yref", yref_N)
+        ##################################################################################
+
+        # solve ocp
+        t = time.time()
+
+        status = acados_solver.solve()
+        if status != 0:
+            print("acados returned status {} in closed loop iteration {}.".format(status, i))
+
+        elapsed = time.time() - t
+        
+        # manage timings
+        tcomp_sum += elapsed
+        if elapsed > tcomp_max:
+            tcomp_max = elapsed
+
+        # get solution
+        u0 = acados_solver.get(0, "u")
+        
+
+        horizon = []
+        for k in range(N):
+            horizon.append(acados_solver.get(k, "x"))
+
+        
+    # current_plot(horizon, ax, round(i*dt,2))    
+
+    x0 = update_state(x0, u0, dt, np.array([0.0,0.0,0]))
+    # x0 = acados_solver.get(1, "x")
+
+    for j in range(nx):
+        simX[i, j] = x0[j]
+    for j in range(nu):
+        simU[i, j] = u0[j]
+
+
+    # update initial condition
+    acados_solver.set(0, "lbx", x0)
+    acados_solver.set(0, "ubx", x0)
+
+    # acados_solver.set("x0", x)
+
+    
+
+# Plot Results
+t = np.linspace(0.0, T, int(T/dt))
+plotRes(simX, simU, t)
+plotTrackProj(simX)
+# current_estim_plot(current, t)
+
+# Print some stats
+print("Average computation time: {}".format(tcomp_sum / int(T/dt)))
+print("Maximum computation time: {}".format(tcomp_max))
+print("Lap time: {}s".format(Tf * Nsim / N))
+# avoid plotting when running on Travis
+if os.environ.get("ACADOS_ON_CI") is None:
+    plt.show()
---
+++ Vanila Ship NMPC2/USV.py
@@ -0,0 +1,66 @@
+import numpy as np
+import math 
+
+def update_state(x_t, u, dt, V):
+
+
+    m = 3980
+    Iz = 19703
+    xdot = 0
+    Yvdot = 0
+    Yrdot = 0
+    Nvdot = 0
+    Nrdot = 0
+    Xu = -50
+    Yv = -200
+    Yr = 0
+    Nv = 0
+    Nr = -1281
+    Xuu = -135
+    Yvv = -2000
+    Nrrr = -3224
+    lr = 4
+    m11 = m - xdot
+    m22 = m - Yvdot 
+    m23 = -Yrdot
+    m32 = -Nvdot
+    m33 = Iz - Nrdot
+
+
+    uu = x_t[0]
+    v = x_t[1]
+    r = x_t[2]
+    psi = x_t[5]
+    Tau_x = x_t[6]
+    Tau_y = x_t[7] 
+
+
+
+    M = np.array([[m11, 0, 0], [0, m22, m23], [0, m32, m33]])
+    
+    Cv = np.array([[0, 0, -m22*v-m23*r], [0, 0, m11*uu], [m22*v+m23*r, -m11*uu, 0]])
+    
+    D = -np.array([[Xu, 0, 0], [0, Yv, Yr], [0, Nv, Nr]])
+    
+    R = np.array([[math.cos(psi), -math.sin(psi), 0], [math.sin(psi), math.cos(psi), 0], [0, 0, 1]])
+
+    uvr = np.array([uu, v, r])
+
+    
+    Tau = np.array([Tau_x, Tau_y, 4*Tau_y])
+    Tau = Tau + np.array([u[0], u[1], 4*u[1]])*dt
+
+    M_inv = np.linalg.inv(M)
+
+    uvr_dot_bM = (Tau - np.dot(Cv, uvr) - np.dot(D, uvr))
+    uvr_dot = np.dot(M_inv, uvr_dot_bM)
+    xypsi_dot = np.dot(R, uvr) + V 
+
+
+    xdot = np.concatenate((uvr_dot, xypsi_dot, u), axis=0)
+    
+
+    x_t_plus_1 = xdot * dt + x_t
+
+    return x_t_plus_1
+
