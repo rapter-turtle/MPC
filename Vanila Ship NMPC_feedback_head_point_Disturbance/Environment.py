1707ed2df49a48052e2a8adaa937f041296a7a90
rapter-turtle <58213518+rapter-turtle@users.noreply.github.com>
2024-06-24 09:43:45 +0900

    Add files via upload

---
+++ Vanila Ship NMPC_feedback_head_point_Disturbance/Environment.py
@@ -0,0 +1,197 @@
+import numpy as np
+import math 
+from USV import *
+from USV_model_update import *
+import time, os
+from simple_acados_settings_dev import *
+from simple_plotFcn import *
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+Tf = 50.0  # prediction horizon
+N = 50  # number of discretization steps
+T = 100  # maximum simulation time[s]
+dt = 0.001
+control_time = 1
+
+
+
+# load model
+constraint, model, acados_solver = acados_settings(Tf, N)
+
+# dimensions
+nx = model.x.size()[0]
+nu = model.u.size()[0]
+ny = nx + nu
+Nsim = int(T * N / Tf)
+
+# initialize data structs
+simX = np.ndarray((int(T/dt), nx+3))
+simU = np.ndarray((int(T/dt), nu))
+
+x_pos = 10.0
+y_pos = 0.0
+
+x_init = model.x0
+state = np.array([0, 0, 0, 0, 0, 0, 0, 0, 10, y_pos, 0])
+nominal_state = np.array([0, 0, 0, 0, 0, 0, 0, 0, 10, y_pos, 0])
+state_before = state
+x0 = x_init
+x0_before = x0
+tcomp_sum = 0
+tcomp_max = 0
+u0 = np.array([0.0, 0.0])
+
+l = 3.5
+m = 3980.0
+Iz = 19703.0
+
+
+x_estim = np.array([l - 10.0 ,-y_pos ,0.0 ,0.0 ,0.0 ,0.0])
+x_estim_before = np.array([l - 10.0 ,-y_pos ,0.0 ,0.0 ,0.0 ,0.0])
+x_error = np.array([0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0])
+param_estim = np.array([0.0, 0.0, 0.0])
+filtered_param = np.array([0.0,0.0, 0.0])
+l1_u = np.array([0.0, 0.0])
+
+sim_x_estim_error = np.ndarray((int(T/dt),6))
+sim_param = np.ndarray((int(T/dt),3))
+sim_l1_con = np.ndarray((int(T/dt),2))
+real = np.ndarray((int(T/dt),6))
+sim_filtered = np.ndarray((int(T/dt),3))
+
+px = 0.0
+py = 0.0
+ppsi = 0.0
+before_px = 0.0
+before_py = 0.0
+before_ppsi = 0.0
+
+# simulate
+for i in range(int(T/dt)):
+    if i%int(control_time/dt) == 0:
+        # print(i)
+        ################################ update reference ################################
+        for j in range(N):
+            yref = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+            acados_solver.set(j, "yref", yref)
+        yref_N = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+        acados_solver.set(N, "yref", yref_N)
+        ##################################################################################
+
+        # solve ocp
+        t = time.time()
+
+        status = acados_solver.solve()
+        if status != 0:
+            print("acados returned status {} in closed loop iteration {}.".format(status, i))
+
+        elapsed = time.time() - t
+        
+        # manage timings
+        tcomp_sum += elapsed
+        if elapsed > tcomp_max:
+            tcomp_max = elapsed
+
+        # get solution
+        u0 = acados_solver.get(0, "u")
+        
+
+        horizon = []
+        for k in range(N):
+            horizon.append(acados_solver.get(k, "x"))
+
+        current_plot(horizon, ax, round(i*dt,2), state)     
+
+
+    l1_u = np.array([0,0])
+    nominal_state, V_ship = update_state(nominal_state, u0, l1_u, dt, np.array([0.0,0.0,0]), np.array([1.0,0.0,0]),i*dt)
+
+    state = nominal_state + np.array([0, 0.25*math.cos(0.5*i*dt), 0, 0, 0.5*math.sin(0.5*i*dt), 0, 0, 0, 0, 0, 0])
+    
+    x0 = np.array([state[0], state[1], state[2], state[3] + l*math.cos(state[5]) - state[8], state[4] + l*math.sin(state[5]) - state[9], state[5], state[6], state[7]])
+
+    ############################## L1 Adaptive control ######################################
+    Gamma_x = 100000
+    Gamma_y = 100000
+    Gamma_psi = 100000
+    w_cutoff = 50
+
+    u_mpc = np.array([state[6], state[7]])
+    x_estim =  estim_update_state(x_estim_before, x_error, u_mpc, filtered_param, dt, np.array([px, py, ppsi]), i*dt) #+ np.dot(Am, x_error)  
+
+    uu = state[0]
+    v = state[1]
+    r = state[2] 
+    psi = state[5]
+    x_l1 = np.array([state[3] + l*math.cos(state[5]) - state[8],                              ## et1
+                     state[4] + l*math.sin(state[5]) - state[9],                              ## et2
+                     uu*math.cos(psi) - v*math.sin(psi) - r*l*math.sin(psi) - V_ship[0],      ## et3
+                     uu*math.sin(psi) + v*math.cos(psi) + r*l*math.cos(psi) - V_ship[1],      ## et4
+                     psi,                                                                     ## psi
+                     r])                                                                      ## r
+    
+    x_error = x_estim - x_l1
+
+    px = Gamma_x*dt*param_dynamics(x_error, before_px, np.array([0, 0, 1, 0, 0, 0]), 0.7) + before_px
+    py = Gamma_y*dt*param_dynamics(x_error, before_py, np.array([0, 0, 0, 1, 0, 0]), 0.7) + before_py
+    ppsi = Gamma_psi*dt*param_dynamics(x_error, before_ppsi, np.array([0, 0, 0, 0, 0, 1]), 1000) + before_ppsi
+          
+
+    param_estim[0] = px
+    param_estim[1] = py
+    param_estim[2] = ppsi
+
+    #################################### Feedback #################################### 
+
+    K_x = np.array([100.0, 10.0])
+    K_y = np.array([100.0, 10.0])
+
+    l1_u[0] = np.dot(np.array([x_error[0], x_error[2]]),K_x)
+    l1_u[1] = np.dot(np.array([x_error[1], x_error[3]]),K_y)
+    
+    #################################### Feedback ####################################
+
+
+    sim_l1_con[i,:] = l1_u
+    sim_param[i,:] = param_estim
+    sim_x_estim_error[i,:] = x_estim
+    real[i,:] = x_l1
+
+    before_px = px
+    before_py = py
+    before_ppsi = ppsi
+    x_estim_before = x_estim
+
+    ##########################################################################################
+
+
+
+    for j in range(nx + 3):
+        simX[i, j] = state[j]
+    for j in range(nu):
+        simU[i, j] = u0[j]
+
+
+    # update initial condition
+    acados_solver.set(0, "lbx", x0)
+    acados_solver.set(0, "ubx", x0)
+
+    # acados_solver.set("x0", x)
+ 
+    
+
+# Plot Results
+t = np.linspace(0.0, T, int(T/dt))
+plotRes(simX, simU, t, sim_l1_con, sim_param, sim_x_estim_error, real, sim_filtered)
+plotTrackProj(sim_x_estim_error, simX, int(T/dt),t)
+# current_estim_plot(current, t)
+
+# Print some stats
+print("Average computation time: {}".format(tcomp_sum / int(T/dt)))
+print("Maximum computation time: {}".format(tcomp_max))
+print("Lap time: {}s".format(Tf * Nsim / N))
+# avoid plotting when running on Travis
+if os.environ.get("ACADOS_ON_CI") is None:
+    plt.show()
---
+++ Vanila Ship NMPC_feedback_head_point_Disturbance/USV.py
@@ -0,0 +1,117 @@
+import numpy as np
+import math 
+
+def update_state(x_t, u, l1_u, dt, V, V_t,t):
+
+
+    m = 3980
+    Iz = 19703
+    xdot = 0
+    Yvdot = 0
+    Yrdot = 0
+    Nvdot = 0
+    Nrdot = 0
+    Xu = -50
+    Yv = -200
+    Yr = 0
+    Nv = 0
+    Nr = -1281
+    Xuu = -135
+    Yvv = -2000
+    Nrrr = -3224
+    lr = 4
+    m11 = m - xdot
+    m22 = m - Yvdot 
+    m23 = -Yrdot
+    m32 = -Nvdot
+    m33 = Iz - Nrdot
+
+
+    uu = x_t[0]
+    v = x_t[1]
+    r = x_t[2]
+    psi = x_t[5]
+    Tau_x = x_t[6]
+    Tau_y = x_t[7]  
+
+
+
+    M = np.array([[m11, 0, 0], [0, m22, m23], [0, m32, m33]])
+    
+    Cv = np.array([[0, 0, -m22*v-m23*r], [0, 0, m11*uu], [m22*v+m23*r, -m11*uu, 0]])
+    
+    D = -np.array([[Xu, 0, 0], [0, Yv, Yr], [0, Nv, Nr]])
+    
+    R = np.array([[math.cos(psi), -math.sin(psi), 0], [math.sin(psi), math.cos(psi), 0], [0, 0, 1]])
+
+    uvr = np.array([uu, v, r])
+
+
+
+    # paramy = 0.03*math.sin(0.1*t)
+    # paramx = 0.03*math.sin(0.1*t)
+    # disturbance_x = (paramy*math.sin(psi) + paramx*math.cos(psi))*m
+    # disturbance_y = (paramy*math.cos(psi) - paramx*math.sin(psi))*m
+
+    disturbance_x = 0.0
+    disturbance_y = 0.0  
+
+    # l1_u = np.array([-(0.0*math.sin(psi) - 0.01*math.sin(0.1*i*dt)*math.cos(psi))*m,-(0.0*math.cos(psi) + 0.01*math.sin(i*dt*0.1)*math.sin(psi))/(1/m + 4*l/Iz)])
+    
+    # print(l1_u[0] + disturbance_x)
+    # print( l1_u[1] + disturbance_y)
+
+    Tau = np.array([Tau_x  + l1_u[0] + disturbance_x, Tau_y + l1_u[1] + disturbance_y, -4*(Tau_y+ l1_u[1])])
+    Tau = Tau + np.array([u[0], u[1], -4*u[1]])*dt
+
+    if Tau[0] > 799.999:
+        Tau[0] = 799.99
+    if Tau[0] < -799.999:
+        Tau[0] = -799.99
+
+    if Tau[1] > 199.999:
+        Tau[1] = 199.99
+    if Tau[1] < -199.999:
+        Tau[1] = -199.99
+
+    M_inv = np.linalg.inv(M)
+
+    uvr_dot_bM = (Tau - np.dot(Cv, uvr) - np.dot(D, uvr))
+    uvr_dot = np.dot(M_inv, uvr_dot_bM)
+    xypsi_dot = np.dot(R, uvr) 
+
+
+    V_y = V_t[1]
+
+
+    # V_y = (0.1*math.cos(0.1*t))
+    V_t[1] = V_y
+
+    xdot = np.concatenate((uvr_dot, xypsi_dot, u, V_t), axis=0)
+    
+
+    x_t_plus_1 = xdot * dt + x_t
+
+    real_psi = x_t_plus_1[5]
+    if real_psi > math.pi:
+        x_t_plus_1[5] -= 2*math.pi
+    if real_psi < -math.pi:
+        x_t_plus_1[5] += 2*math.pi
+        
+    
+    if x_t_plus_1[6] > 799.999:
+        x_t_plus_1[6] = 799.99
+    if x_t_plus_1[6] < -799.999:
+        x_t_plus_1[6] = -799.99
+
+    if x_t_plus_1[7] > 199.999:
+        x_t_plus_1[7] = 199.99
+    if x_t_plus_1[7] < -199.999:
+        x_t_plus_1[7] = -199.99
+
+
+
+    V_ship = np.array([1.0, V_y, 0.0])
+
+    return x_t_plus_1, V_ship
+
